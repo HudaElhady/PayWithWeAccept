fc7cbfd0714f9eb60a472afd8f426b75029313e6
huda_elhady1 <huda.elhaty@raye7.com>
2019-06-21 18:24:48 +0200

    adding new project

---
+++ PayWithWeAccept/PayWithWeAccept/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayWithWeAccept
+//
+//  Created by huda elhady on 6/21/19.
+//  Copyright © 2019 huda.elhady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
